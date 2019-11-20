c5f0f3f35451bf5aadf33db0f76c3395f46096da
yuta.arai <yutaarairai@icloud.com>
2019-11-20 21:13:52 +0900

    Modify null control

--- src/controllers/EditServlet.java
+++ src/controllers/EditServlet.java
@@ -45,8 +45,9 @@ public class EditServlet extends HttpServlet {
         request.setAttribute("_token", request.getSession().getId());
 
         // メッセージIDをセッションスコープに登録
+        if(t != null) {
         request.getSession().setAttribute("task_id", t.getId());
-
+        }
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/edit.jsp");
         rd.forward(request, response);
     }
